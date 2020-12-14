e506734f695046aefc57bc1ad110a81c57b4244c
livialestingi <livia.lestingi@polimi.it>
2020-12-14 12:09:47 +0100

    minor fixes for multiple log plotting/learning

--- it/polimi/hri_learn/learning_pg.py
+++ it/polimi/hri_learn/learning_pg.py
@@ -1,5 +1,5 @@
 from typing import List
-
+import os
 import mgrs.ha_mgr as ha_mgr
 import mgrs.sig_mgr as sig_mgr
 import pltr.sig_pltr as sig_pltr
@@ -7,31 +7,37 @@ import pltr.ha_pltr as ha_pltr
 from domain.hafeatures import HybridAutomaton, LOCATIONS, Edge
 from domain.sigfeatures import SignalPoint, ChangePoint
 
-LOG_PATH = "resources/sim_logs/humanFatigue.log"
+LOG_PATH = "resources/sim_logs/sim{}"
+FTG_LOG = 'humanFatigue.log'
+POS_LOG = 'humanPosition.log'
 HUM_ID = 1
 
-f = open(LOG_PATH, 'r')
-lines = f.readlines()
-lines = list(filter(lambda l: len(l) > 2, lines))
-lines = list(filter(lambda l: l.split('#')[1] == 'hum' + str(HUM_ID), lines))
+SIM_ID = 1
 
-entries: List[SignalPoint] = []
-for line in lines:
-    fields = line.split('#')
-    entries.append(SignalPoint(float(fields[0]), int(fields[1].replace('hum', '')), float(fields[2])))
+while os.path.isdir(LOG_PATH.format(SIM_ID)):
+    f = open(LOG_PATH.format(SIM_ID) + '/' + FTG_LOG, 'r')
+    lines = f.readlines()
+    lines = list(filter(lambda l: len(l) > 2, lines))
+    lines = list(filter(lambda l: l.split(':')[1] == 'hum' + str(HUM_ID), lines))
 
-change_pts: List[ChangePoint] = sig_mgr.identify_change_pts(entries)
+    entries: List[SignalPoint] = []
+    for line in lines:
+        fields = line.split(':')
+        entries.append(SignalPoint(float(fields[0]), int(fields[1].replace('hum', '')), float(fields[2])))
 
-# for entry in entries:
-#     print(entry)
-#     change_pt = list(filter(lambda pt: pt.dt.t_min == entry.timestamp, change_pts))
-#     if len(change_pt) > 0:
-#         print(change_pt[0])
+    change_pts: List[ChangePoint] = sig_mgr.identify_change_pts(entries)
 
-sig_pltr.plot_sig(entries, change_pts)
+    # for entry in entries:
+    #     print(entry)
+    #     change_pt = list(filter(lambda pt: pt.dt.t_min == entry.timestamp, change_pts))
+    #     if len(change_pt) > 0:
+    #         print(change_pt[0])
 
-HUM_HA: HybridAutomaton = HybridAutomaton(LOCATIONS, [])
-edges: List[Edge] = ha_mgr.identify_edges(change_pts)
-HUM_HA.set_edges(edges)
+    sig_pltr.plot_sig(entries, change_pts)
 
-ha_pltr.plot_ha(HUM_HA, 'ha_1')
+    HUM_HA: HybridAutomaton = HybridAutomaton(LOCATIONS, [])
+    edges: List[Edge] = ha_mgr.identify_edges(change_pts)
+    HUM_HA.set_edges(edges)
+
+    ha_pltr.plot_ha(HUM_HA, 'ha_{}'.format(SIM_ID))
+    SIM_ID += 1
