7770c7b2e20f73e606a1baa887a986650143bc03
livialestingi <livia.lestingi@polimi.it>
2021-03-27 18:18:15 +0100

    added rate estimation from segments

--- it/polimi/hri_learn/learning_pg.py
+++ it/polimi/hri_learn/learning_pg.py
@@ -1,8 +1,12 @@
+import math
 import warnings
 from typing import List
-import mgrs.sig_mgr
 
 import matplotlib.pyplot as plt
+
+import mgrs.sig_mgr as sig_mgr
+from domain.sigfeatures import SignalPoint, TimeInterval
+from domain.hafeatures import LocLabels
 
 
 def find_chg_pts(values: List[float]):
@@ -87,9 +91,68 @@ for (i, sim) in enumerate(ftg_entries):
     idle_pts = [float(x.split(' ')[1]) for x in idle_entries[i][1:len(sim) - 1] if len(x.split(' ')) > 1]
     plt.plot(t, idle_pts, 'g--', linewidth=1)
 
-    [plt.plot(t[n], mov_pts[n], 'bx', markersize=12) for n in chg_pts[i]]
+    [plt.plot(t[n], 1, 'bx', markersize=12) for n in chg_pts[i]]
     plt.show()
+
+'''
+SPLIT SEGMENTS AND SET LABEL
+'''
+segments = []
+labels = []
+for i in range(len(chg_pts)):
+    sim_segm = []
+    sim_labels = []
+    prev = 0.0
+    for pt in chg_pts[i]:
+        curr = float(mov_entries[i][pt].split(' ')[0])
+        if float(mov_entries[i][pt - 1].split(' ')[1]) == 0.0:
+            sim_labels.append(LocLabels.IDLE)
+        else:
+            sim_labels.append(LocLabels.BUSY)
+        sig_pts = list(filter(lambda e: len(e.split(' ')) > 1 and
+                                        prev <= float(e.split(' ')[0]) < curr, ftg_entries[i]))
+        sig_pts = list(map(lambda x: SignalPoint(float(x.split(' ')[0]), 1, float(x.split(' ')[1])), sig_pts))
+        sim_segm.append(sig_pts)
+        prev = curr
+
+    if float(mov_entries[i][- 2].split(' ')[1]) == 0.0:
+        sim_labels.append(LocLabels.IDLE)
+    else:
+        sim_labels.append(LocLabels.BUSY)
+    curr = float(ftg_entries[i][-2].split(' ')[0])
+    sig_pts = list(filter(lambda e: len(e.split(' ')) > 1 and
+                                    prev <= float(e.split(' ')[0]) < curr, ftg_entries[i]))
+    sig_pts = list(map(lambda x: SignalPoint(float(x.split(' ')[0]), 1, float(x.split(' ')[1])), sig_pts))
+    sim_segm.append(sig_pts)
+
+    segments.append(sim_segm)
+    labels.append(sim_labels)
 
 '''
 ESTIMATE RATES FOR IDENTIFIED SEGMENTS
 '''
+est_rates = []
+for sim in range(len(segments)):
+    rates = []
+    for segment in segments[sim]:
+        t = [pt.timestamp for pt in segment]
+        x = [pt.value for pt in segment]
+        try:
+            dt = TimeInterval(segment[0].timestamp, segment[-1].timestamp)
+            params, _, _ = sig_mgr.n_predictions(segment, dt, 10, show_formula=False)
+            est_rate = -math.log(params[1]) if params[1] != 0.0 else 0.0
+            # print('{} {}'.format(est_rate, labels[sim][segments[sim].index(segment)]))
+            rates.append(est_rate)
+        except ValueError:
+            rates.append(0.0)
+            print("Not enough data pts ({})".format(len(segment)))
+    est_rates.append(rates)
+
+lambdas = [rate for sim in est_rates for rate in sim if labels[est_rates.index(sim)][sim.index(rate)] == LocLabels.BUSY]
+mus = [rate for sim in est_rates for rate in sim if labels[est_rates.index(sim)][sim.index(rate)] == LocLabels.IDLE]
+
+avg_lambda = sum(lambdas)/len(lambdas)
+print(avg_lambda)
+
+avg_mu = sum(mus)/len(mus)
+print(avg_mu)
