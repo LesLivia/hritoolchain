d3f3bca7f19994067ae440b25407c7eab73e0da2
livialestingi <livia.lestingi@polimi.it>
2021-07-11 16:58:40 +0200

    wip access exp metrics

--- acc_exp_data.py
+++ acc_exp_data.py
@@ -9,7 +9,6 @@ def parse_file(path: str):
     # 1.00: 29.9286
     t = [float(line.split(':')[0]) for line in lines if line != '\n']
     vals = [float(line.split(':')[-1]) for line in lines if line != '\n']
-    pts = [(x, vals[i]) for (i, x) in enumerate(t)]
     return t, vals
 
 
@@ -23,20 +22,48 @@ OUT_FILE = '/outfile.txt'
 
 print('FOUND {} SIMULATION TO ANALYZE'.format(len(SIM_LOGS_DIRS)))
 
-### PLOT FTG CURVES
+# PLOT FTG CURVES
 
-plt.figure()
+ftg_t, ftg_pts = zip(*[parse_file(s + FTG_FILE) for s in SIM_LOGS_DIRS])
 
-for sim in SIM_LOGS_DIRS:
-    ftg_t, ftg_pts = parse_file(sim + FTG_FILE)
-    plt.plot(ftg_t, ftg_pts)
+plt.figure(figsize=(10, 5))
+plt.xlabel('t[s]', fontsize=18)
+plt.ylabel('F[0-1]', fontsize=18)
 
+for (i, t) in enumerate(ftg_t):
+    if i == 0:
+        plt.plot(t, ftg_pts[i], '--', color='black', linewidth=0.25, alpha=0.5,
+                 label='simulations ({})'.format(len(SIM_LOGS_DIRS)))
+    else:
+        plt.plot(t, ftg_pts[i], '--', color='black', linewidth=0.25, alpha=0.5)
+
+avg_t = [t for t in ftg_t if len(t) == max([len(x) for x in ftg_t])][0]
+avg_ftg = []
+for i in range(max([len(x) for x in ftg_t])):
+    sum_f = 0
+    n = 0
+    for f in ftg_pts:
+        if len(f) > i:
+            sum_f += f[i]
+            n += 1
+    if n > 10:
+        avg_ftg.append(sum_f / n)
+    else:
+        avg_ftg.append(avg_ftg[-1])
+plt.plot(avg_t, avg_ftg, color='red', linewidth=2.0, label='average')
+
+HUM_SWITCH = 51.0
+plt.vlines([HUM_SWITCH], [0.0], [0.02], color='blue', label='hum. switch')
+plt.legend()
 plt.show()
 
-plt.figure()
+MAX_EXP_1 = max(avg_ftg[:avg_t.index(HUM_SWITCH)])
+print('HUM.1 MAX EXPECTED FATIGUE: {}'.format(MAX_EXP_1))
+MAX_EXP_2 = max(avg_ftg[avg_t.index(HUM_SWITCH):])
+print('HUM.2 MAX EXPECTED FATIGUE: {}'.format(MAX_EXP_2))
 
+plt.figure(figsize=(10, 5))
 for sim in SIM_LOGS_DIRS:
     chg_t, chg_pts = parse_file(sim + CHG_FILE)
-    plt.plot(chg_t, chg_pts)
-
+    plt.plot(chg_t, chg_pts, color='orange', linewidth=0.5)
 plt.show()
